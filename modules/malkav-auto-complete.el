50734cc30d4f5092aeb492c7ef7e6faea7d9aa1e
Paulo Fagundes <paulohefagundes@gmail.com>
2019-11-06 11:08:18 -0700

    remove free pascal module and disable ruby module

--- modules/malkav-auto-complete.el
+++
@@ -1,8 +0,0 @@
-;; auto-complete
-(require 'auto-complete-config)
-(ac-config-default)
-(add-to-list 'ac-dictionary-directories
-             (expand-file-name "dict" malkav-vendor-dir)) ;; add custom dictionary folder
-(add-to-list 'ac-modes 'opascal-mode) ;; enable AC automatically for specific modes
-
-(provide 'malkav-auto-complete)
