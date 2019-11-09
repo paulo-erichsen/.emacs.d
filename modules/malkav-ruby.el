ff510075d6231afece909d80a438c9125c130924
Paulo Fagundes <paulohefagundes@gmail.com>
2019-11-09 09:21:00 -0700

    update ruby-mode from prelude

--- modules/malkav-ruby.el
+++ modules/malkav-ruby.el
@@ -34,46 +34,23 @@
 
 (require 'malkav-programming)
 
-(malkav-require-packages '(ruby-tools inf-ruby yari rubocop rspec-mode rbenv bundler))
-(require 'ruby-tools)
-
-;; Rake files are ruby, too, as are gemspecs, rackup files, and gemfiles.
-(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.cap\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.thor\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rabl\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Thorfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Podfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.podspec\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Puppetfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Berksfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Appraisals\\'" . ruby-mode))
+(malkav-require-packages '(inf-ruby yari rubocop rspec-mode))
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
 
 (define-key 'help-command (kbd "R") 'yari)
 
-(eval-after-load 'ruby-mode
-  '(progn
-     (defun malkav-ruby-mode-defaults ()
-       (inf-ruby-minor-mode +1)
-       (ruby-tools-mode +1)
-       ;; CamelCase aware editing operations
-       (subword-mode +1))
+(with-eval-after-load 'ruby-mode
+  (defun malkav-ruby-mode-defaults ()
+    (inf-ruby-minor-mode +1)
+    ;; CamelCase aware editing operations
+    (subword-mode +1))
 
-     (setq malkav-ruby-mode-hook 'malkav-ruby-mode-defaults)
+  (setq malkav-ruby-mode-hook 'malkav-ruby-mode-defaults)
 
-     (add-hook 'ruby-mode-hook (lambda ()
-                                 (run-hooks 'malkav-ruby-mode-hook)))))
+  (add-hook 'ruby-mode-hook (lambda ()
+                              (run-hooks 'malkav-ruby-mode-hook))))
 
 (provide 'malkav-ruby)
 ;;; malkav-ruby.el ends here
